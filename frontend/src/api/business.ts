eab53b359ef2807201bb8480b58eb38819b82cb2
brianctb <brianlee1188@gmail.com>
2026-04-05 01:58:32 -0700

    add new business api

--- frontend/src/api/business.ts
+++ frontend/src/api/business.ts
@@ -1,7 +1,9 @@
 import { apiClient } from "./client";
-import { BusinessCategoryResponse } from "@/types/business";
+import { BusinessCategoryResponse, BusinessesResponse } from "@/types/business";
 
 export const businessApi = {
     getMenu: () =>
         apiClient.get<BusinessCategoryResponse[]>("/business/menu"),
+    getBusinesses: (business_type: string, lng: number, lat: number) =>
+        apiClient.get<BusinessesResponse>("/locations/businesses", { business_type, lng, lat }),
 };
