04e5676bb282143268349ec116beefed4811925b
Brian Lee <clee689@my.bcit.ca>
2026-04-07 22:46:56 -0700

    changes boundary on map

--- frontend/src/configs/map.ts
+++ frontend/src/configs/map.ts
@@ -1,15 +1,15 @@
 export const MAP_CONFIG = {
     vancouver: {
         bounds: [
-            [-123.5, 49.0],
-            [-122.2, 49.6],
+            [-123.30, 49.10],
+            [-122.75, 49.38],
         ] as [[number, number], [number, number]],
 
 
         center: {
             longitude: -123.1207,
             latitude: 49.2827,
-            zoom: 11,
+            zoom: 13,
         },
     },
 
